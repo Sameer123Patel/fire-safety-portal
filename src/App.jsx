13ba58ec7a7b34bd13c771c6c82272d2e8d5bb6e
21btrse044 <136232306+Sameer123Patel@users.noreply.github.com>
2025-05-06 21:31:41 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,1 @@
+/* Main App component will be placed here */
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import FireSafetyPortal from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FireSafetyPortal />
+  </React.StrictMode>
+);
